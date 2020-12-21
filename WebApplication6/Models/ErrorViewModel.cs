4b4a597b326b03a00ab933085f36abb9440b7726
Aimee Cao <v-liucao@microsoft.com>
2020-12-21 07:34:10 +0000

    Add project files.

---
+++ WebApplication6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace WebApplication6.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
